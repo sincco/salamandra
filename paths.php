03ee44a3f6eb6c5d5953c14e7fdd9ac23dc4a4f2
ivan miranda <ivan@pengostores.com>
2016-05-27 18:57:31 -0500

    Avances 20160527

---
+++ paths.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Paths for Framework
+ */
+define('PATH_ROOT', __DIR__);
+define('PATH_ERROR_TEMPLATE', PATH_ROOT . '/errors');
+define('PATH_CACHE', PATH_ROOT . '/var/cache');
+define('PATH_LOGS', PATH_ROOT . '/var/log');
+define('PATH_CONFIG', PATH_ROOT . '/etc/config');
+define('PATH_SESSION', PATH_ROOT . '/var/session');
