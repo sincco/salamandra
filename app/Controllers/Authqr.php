2f0afca35798be70d4d1d6259bbab83d02c1d1f2
ivan miranda <ivan@pengostores.com>
2016-06-27 13:27:41 -0500

    Avances 20160627

--- app/Controllers/Authqr.php
+++ app/Controllers/Authqr.php
@@ -10,11 +10,17 @@ class AuthqrController extends Sincco\Sfphp\Abstracts\Controller {
 	private $llave = 'GEAKO2IJW4PKLBXF';
 
 	public function index() {
-		$llave = "GEAKO2IJW4PKLBXF";
+		$this->helper( 'UsersAccount' )->checkLogin();
+		$view = $this->newView( 'AuthQR' );
+		$view->menus = $this->helper( 'UsersAccount' )->createMenus();
+		$view->render();
+	}
+
+	public function apiValidar() {
 		$tiempo = floor(time() / 30);
-		$_auth = new Google2Step();
-		$_codigo = $_POST['codigo'];
-		if($_auth->validaCodigo($llave,$_codigo))
+		$codigo = $this->getParam( 'codigo' );
+		var_dump($codigo);die();
+		if($_auth->validaCodigo($this->llave, $_codigo))
 			$this->_vista->clientes = $this->_modelo->actualiza($_POST['cliente'],$_POST['status']);
 		else
 			echo "ERROR";
@@ -28,8 +34,10 @@ class AuthqrController extends Sincco\Sfphp\Abstracts\Controller {
 	}
 
 	public function config() {
+		$this->helper( 'UsersAccount' )->checkLogin();
 		$tiempo = floor(time() / 30);
 		$view = $this->newView( 'AuthQRConfig' );
+		$view->menus = $this->helper( 'UsersAccount' )->createMenus();
 		$view->qrUrl = $this->helper( 'Google2Step' )->urlQR( 'sae', 'suhner.com', $this->llave );
 		$view->codigo = $this->helper( 'Google2Step' )->generaCodigo( $this->llave, $tiempo);
 		$view->render();
