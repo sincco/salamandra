e3673a4aacb55b7b3d576b343738779024a77506
Iván Miranda <ivan.miranda@sincco.com>
2016-12-14 14:49:11 -0600

    Avances20161214

--- app/Api/Controllers/V1.php
+++ app/Api/Controllers/V1.php
@@ -25,14 +25,14 @@ class V1Controller extends Sincco\Sfphp\Abstracts\Controller {
 	public function unidades() {
 		switch ($this->getRequest()['method']) {
 			case 'POST':
-				$model = $this->getModel('Catalogos\Unidades');
+				$model = $this->getModel('Catalogo\Unidades');
 				$data = $this->getParams('data');
 				unset($data['idUnidad']);
 				$id = $model->insert($data);
 				new Response('json', ['respuesta'=>$id]);
 				break;
 			case 'PUT':
-				$model = $this->getModel('Catalogos\Unidades');
+				$model = $this->getModel('Catalogo\Unidades');
 				$data = $this->getParams('data');
 				$where = ['idUnidad'=>$data['idUnidad']];
 				unset($data['idUnidad']);
@@ -48,14 +48,14 @@ class V1Controller extends Sincco\Sfphp\Abstracts\Controller {
 	public function operadores() {
 		switch ($this->getRequest()['method']) {
 			case 'POST':
-				$model = $this->getModel('Catalogos\Operadores');
+				$model = $this->getModel('Catalogo\Operadores');
 				$data = $this->getParams('data');
 				unset($data['idOperador']);
 				$id = $model->insert($data);
 				new Response('json', ['respuesta'=>$id]);
 				break;
 			case 'PUT':
-				$model = $this->getModel('Catalogos\Operadores');
+				$model = $this->getModel('Catalogo\Operadores');
 				$data = $this->getParams('data');
 				$where = ['idOperador'=>$data['idOperador']];
 				unset($data['idOperador']);
