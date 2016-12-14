327a03d6dd89faa832544edbb3aa3f3519f881e8
Iván Miranda <ivan.miranda@sincco.com>
2016-12-14 10:11:19 -0600

    Avances20161214

--- app/Api/Controllers/V1.php
+++ app/Api/Controllers/V1.php
@@ -20,5 +20,51 @@ class V1Controller extends Sincco\Sfphp\Abstracts\Controller {
 				# code...
 				break;
 		}
-	} 
+	}
+
+	public function unidades() {
+		switch ($this->getRequest()['method']) {
+			case 'POST':
+				$model = $this->getModel('Catalogos\Unidades');
+				$data = $this->getParams('data');
+				unset($data['idUnidad']);
+				$id = $model->insert($data);
+				new Response('json', ['respuesta'=>$id]);
+				break;
+			case 'PUT':
+				$model = $this->getModel('Catalogos\Unidades');
+				$data = $this->getParams('data');
+				$where = ['idUnidad'=>$data['idUnidad']];
+				unset($data['idUnidad']);
+				$model->update($data, $where);
+				new Response('json', ['respuesta'=>true]);
+				break;
+			default:
+				new Response('htmlstatuscode', 'Operacion no soportada');
+				break;
+		}
+	}
+
+	public function operadores() {
+		switch ($this->getRequest()['method']) {
+			case 'POST':
+				$model = $this->getModel('Catalogos\Operadores');
+				$data = $this->getParams('data');
+				unset($data['idOperador']);
+				$id = $model->insert($data);
+				new Response('json', ['respuesta'=>$id]);
+				break;
+			case 'PUT':
+				$model = $this->getModel('Catalogos\Operadores');
+				$data = $this->getParams('data');
+				$where = ['idOperador'=>$data['idOperador']];
+				unset($data['idOperador']);
+				$model->update($data, $where);
+				new Response('json', ['respuesta'=>true]);
+				break;
+			default:
+				new Response('htmlstatuscode', 'Operacion no soportada');
+				break;
+		}
+	}
 }
