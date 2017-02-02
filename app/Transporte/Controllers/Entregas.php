38ba66ff1f1a98383f21d8f985bf435f61ecbe3b
Iván Miranda <ivan.miranda@sincco.com>
2017-02-02 10:58:41 -0600

    Avances20170202

--- app/Transporte/Controllers/Entregas.php
+++ app/Transporte/Controllers/Entregas.php
@@ -25,4 +25,22 @@ class EntregasController extends Sincco\Sfphp\Abstracts\Controller {
 		$view->menus = $this->helper('UsersAccount')->createMenus();
 		$view->render();
 	}
+
+	public function entregados()
+	{
+		$this->helper('UsersAccount')->checkLogin();
+		$view = $this->newView('Transporte\Entregados');
+		$entregas = $this->getModel('Salamandra')->entregas()
+			->fields('pedido')
+			->fields('fechaEntrega')
+			->join('unidades uni', 'uni.idUnidad = maintable.idUnidad')
+			->join('operadores ope', 'ope.idOperador = maintable.idOperador')
+			->where('fechaEntrega',date('Y-m-d'))
+			->where('entregado',1)
+			->getData();
+		$entregas = array_map("unserialize", array_unique(array_map("serialize", $entregas)));
+		$view->entregas = $entregas;
+		$view->menus = $this->helper('UsersAccount')->createMenus();
+		$view->render();
+	}
 }
