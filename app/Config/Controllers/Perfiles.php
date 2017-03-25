9c1578ef45a9c78cf13a3d71794ae89d7f11e58d
Iván Miranda <ivan.miranda@sincco.com>
2017-03-24 18:26:44 -0600

    Seguridad

---
+++ app/Config/Controllers/Perfiles.php
@@ -0,0 +1,25 @@
+<?php
+
+use \Sincco\Sfphp\XML;
+use \Sincco\Sfphp\Response;
+use \Sincco\Tools\Debug;
+use \Sincco\Sfphp\Messages;
+
+/**
+ * Dashboard del sistema
+ */
+class PerfilesController extends Sincco\Sfphp\Abstracts\Controller {
+	
+	/**
+	 * Acción por default
+	 * @return none
+	 */
+	public function index() {
+		$view = $this->newView('Pefiles');
+		$view->paneles = $paneles;
+		$view->menus = $this->helper('UsersAccount')->createMenus();
+		$view->filtros = $paramsFront;
+		$view->compras = $this->getModel('Dashboard')->getCompras($fechaInicio, $fechaFin);
+		$view->render();
+	}
+}
